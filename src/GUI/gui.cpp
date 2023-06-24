e75b887e76ab236044efb28ed466ac192693683d
Swofty <admin@swofty.net>
2023-06-24 12:25:31 +1000

    Added minsize and disable

--- src/GUI/gui.cpp
+++ src/GUI/gui.cpp
@@ -508,7 +508,7 @@ void GUI::tools() {
 			ImGuiFileDialog::Instance()->OpenDialog("MergeReplay", "Choose File", ".echo,.bin", ".echo/");
 		}
 
-		if (ImGuiFileDialog::Instance()->Display("MergeReplay"))
+		if (ImGuiFileDialog::Instance()->Display("MergeReplay", ImGuiWindowFlags_NoCollapse, ImVec2(500, 200)))
 		{
 			if (ImGuiFileDialog::Instance()->IsOk())
 			{
@@ -705,7 +705,7 @@ void GUI::conversion() {
 
 		auto& style = ImGui::GetStyle();
 
-		if (current_option == "Osu") { // I fucking hate the way imgui does disabling
+		if (current_option == "Osu") {
 			ImGui::BeginDisabled();
 
 			if (ImGui::Button("Import"))
@@ -731,7 +731,7 @@ void GUI::conversion() {
 			}
 		}
 
-		if (ImGuiFileDialog::Instance()->Display("ConversionImport"))
+		if (ImGuiFileDialog::Instance()->Display("ConversionImport", ImGuiWindowFlags_NoCollapse, ImVec2(500, 200)))
 		{
 			// action if OK
 			if (ImGuiFileDialog::Instance()->IsOk())
@@ -995,7 +995,7 @@ void GUI::main() {
 			logic.sort_inputs();
 		}
 
-		if (ImGuiFileDialog::Instance()->Display("ImportNormal"))
+		if (ImGuiFileDialog::Instance()->Display("ImportNormal", ImGuiWindowFlags_NoCollapse, ImVec2(500, 200)))
 		{
 			// action if OK
 			if (ImGuiFileDialog::Instance()->IsOk())
