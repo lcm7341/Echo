037079bd72555999176b5ea259a80acbfc51a085
lcm7341 <krx132@gmail.com>
2023-11-30 22:30:59 -0600

    fsdgasa Fuck!

--- src/GUI/gui.hpp
+++ src/GUI/gui.hpp
@@ -31,8 +31,6 @@ public:
 
 	void clickbot();
 
-	bool ToggleButton(const char* str_id, bool* v, std::string id="");
-
 	void show_keybind_prompt(const std::string& buttonName);
 
 	void import_theme(std::string path);
@@ -73,17 +71,6 @@ public:
 
 	ImVec4 player_1_button_color = { 0.18, 0.89, 0.7, 0.54 };
 	ImVec4 player_2_button_color = { 0.18, 0.89, 0.46, 0.54 };
-	ImVec4 input_selected_color_p1 = { 0.2, 0.9, 0.8, 0.54 };
-	ImVec4 input_selected_color_p2 = { 0.2, 0.9, 0.8, 0.54 };
-
-	ImVec4 toggle_unchecked_bg = { 0.85f, 0.85f, 0.85f, 1.0f };
-	ImVec4 toggle_unchecked_bg_hover = { 0.78f, 0.78f, 0.78f, 1.0f };
-
-	ImVec4 toggle_checked_bg = { 0.85f, 0.85f, 0.85f, 1.0f };
-	ImVec4 toggle_checked_bg_hover = { 0.78f, 0.78f, 0.78f, 1.0f };
-
-	ImVec4 toggle_circle = { 1, 1, 1, 1 };
-	ImVec4 toggle_circle_hover = { 1, 1, 1, 1 };
 
 	ImVec2 main_pos;
 	ImVec2 tools_pos;
